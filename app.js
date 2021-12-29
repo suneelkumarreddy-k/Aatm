b1f65137a1e8adeb14d27a85621ab6ca9a59aec6
suneelkumarreddy-k <67097067+suneelkumarreddy-k@users.noreply.github.com>
2021-12-29 14:08:52 +0530

    Update app.js
    Code changed

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let age = document.getElementById("age").value;
 let avatar = age/karma;
 let aham = avatar/age;
 document.getElementById("avts").innerHTML = Math.ceil(avatar);
-document.getElementById("aham").innerHTML = Math.ceil(aham);
+document.getElementById("aham").innerHTML = aham;
 var end = performance.now();
 var wpn = end-start;
 alert("Calculated in "+wpn+" ms 😎");
